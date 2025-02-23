da70352301975ecb279e2180e201b372a90d6c42
Manikandan61203 <manikandan61203@gmail.com>
2025-02-23 22:39:18 +0530

    first

--- Mapping/Mapping/src/main/java/com/example/Mapping/Model/Customer.java
+++ Mapping/Mapping/src/main/java/com/example/Mapping/Model/Customer.java
@@ -15,13 +15,53 @@ import java.util.List;
 @Entity
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
     private String gender;
 
-    @OneToMany(targetEntity = Product.class,cascade = CascadeType.ALL)
-    @JoinColumn(name="cusPro",referencedColumnName = "id")
+    @OneToMany(targetEntity = Product.class,cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Product> products;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
+
--- Mapping/Mapping/src/main/java/com/example/Mapping/Model/Product.java
+++ Mapping/Mapping/src/main/java/com/example/Mapping/Model/Product.java
@@ -2,10 +2,13 @@ package com.example.Mapping.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
 
 @Data
 @AllArgsConstructor
@@ -18,4 +21,48 @@ public class Product {
     private String pName;
     private int qty;
     private int price;
+
+    @ManyToOne
+    @JoinColumn(name = "cusId")
+    private Customer customer;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int getpId() {
+        return pId;
+    }
+
+    public void setpId(int pId) {
+        this.pId = pId;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
